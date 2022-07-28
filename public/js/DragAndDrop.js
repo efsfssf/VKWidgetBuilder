a22953b1e2d844f9a03ed1ca7c0442b7c28519e5
MrDandomi <bezzubik46@gmail.com>
2022-07-28 10:00:54 +0300

    Добавлено перетаскивание блоков

--- public/js/DragAndDrop.js
+++ public/js/DragAndDrop.js
@@ -38,35 +38,59 @@ var windowOnloadAdd = function (event) {
     };
 
     tasksListElement.addEventListener(`dragover`, (evt) => {
-    // Разрешаем сбрасывать элементы в эту область
-    evt.preventDefault();
-    
-    // Находим перемещаемый элемент
-    const activeElement = tasksListElement.querySelector(`.selected`);
-    // Находим элемент, над которым в данный момент находится курсор
-    const currentElement = evt.target;
-    // Проверяем, что событие сработало:
-    // 1. не на том элементе, который мы перемещаем,
-    // 2. именно на элементе списка
-    const isMoveable = activeElement !== currentElement &&
-        currentElement.classList.contains(`tasks__item`);
-    
-    // Если нет, прерываем выполнение функции
-    if (!isMoveable) {
-        return;
-    }
-    
-    // Находим элемент, перед которым будем вставлять
-    const nextElement = getNextElement(evt.clientY, currentElement);
+        // Разрешаем сбрасывать элементы в эту область
+        evt.preventDefault();
+        
+        // Находим перемещаемый элемент
+        const activeElement = tasksListElement.querySelector(`.selected`) || tasksListElementMenu.querySelector(`.selected`);
+        // Находим элемент, над которым в данный момент находится курсор
+        const currentElement = evt.target;
+        // Проверяем, что событие сработало:
+        // 1. не на том элементе, который мы перемещаем,
+        // 2. именно на элементе списка
+        const isMoveable = activeElement !== currentElement &&
+            currentElement.classList.contains(`tasks__item`) || currentElement.classList.contains(`tasks__itemMenu`);
+        
+        // Если нет, прерываем выполнение функции
+        if (!isMoveable) {
+            return;
+        }
+        
+        // Находим элемент, перед которым будем вставлять
+        const nextElement = getNextElement(evt.clientY, currentElement);
 
-    if (
-        nextElement && 
-        activeElement === nextElement.previousElementSibling ||
-        activeElement === nextElement
-      ) {
-        return;
-    }
-    // Вставляем activeElement перед nextElement
-    tasksListElement.insertBefore(activeElement, nextElement);
+        if (
+            nextElement && 
+            activeElement === nextElement.previousElementSibling ||
+            activeElement === nextElement
+        ) {
+            return;
+        }
+        // Вставляем activeElement перед nextElement
+        console.log(activeElement);
+        console.log(nextElement);
+        tasksListElement.insertBefore(activeElement, nextElement);
     });
+
+
+
+    // МЕНЮ
+    const tasksListElementMenu = document.getElementsByClassName('tasks__list')[0];
+    
+    const taskElementsMenu = tasksListElementMenu.querySelectorAll(`.tasks__itemMenu`);
+
+    // Перебираем все элементы списка и присваиваем нужное значение
+    for (const task of taskElementsMenu) {
+        task.draggable = true;
+    }
+
+    tasksListElementMenu.addEventListener(`dragstart`, (evt) => {
+        evt.target.classList.add(`selected`);
+    })
+    
+    tasksListElementMenu.addEventListener(`dragend`, (evt) => {
+        evt.target.classList.remove(`selected`);
+    });
+
+    
  });
